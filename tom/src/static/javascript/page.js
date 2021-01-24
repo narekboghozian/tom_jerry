112b5cbee0e8675cab03ae67c190f3c1b6f3a10b
narekboghozian <66885970+narekboghozian@users.noreply.github.com>
2021-01-23 22:22:33 -0800

    .

--- tom/src/static/javascript/page.js
+++ tom/src/static/javascript/page.js
@@ -4,18 +4,21 @@
 function load_page(pageNum) {
 
   var loggedIn;
-  document.cookie = "hello: user";
-  let user = document.cookie;
+  // document.cookie = "hello=user";
+  //let user = document.cookie;
+  let user = getCookie("user");
   console.log(user);
-  if(user==''){
+  if(user==null){
     loggedIn = false;
     console.log("Not logged in");
+    display_login();
   }
   else{
     loggedIn = true;
   }
   // Make navbar
 
+  // ask server for forum
 
 }
 
@@ -27,7 +30,21 @@ function nav_bar(){
 
 
 function display_login(){
-  let body = document.getElementsByTagName('body');
-  let login_box = document.createElement("div");
+  var body = document.getElementsByTagName("body")[0];
+  var login_box = document.createElement("div");
   login_box.className = "login_box";
+  var entry = document.createElement("input");
+  var login_box_text = document.createElement("div");
+  login_box_text.className = "login_box_text";
+  login_box_text.innerHTML = "Login";
+  body.appendChild(login_box);
+  login_box.appendChild(login_box_text);
+}
+
+function getCookie(name) {
+  let ret = null;
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) ret =  parts.pop().split(';').shift();
+  return ret;
 }
